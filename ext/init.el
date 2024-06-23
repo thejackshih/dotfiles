1dd6c0e4440649bfad3543c53304b8b937256ddd
Jack Shih <randomdize@gmail.com>
2024-06-23 23:50:06 +0800

    remove depracated org package archive

--- ext/init.el
+++ ext/init.el
@@ -1,5 +1,4 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			   ("org" . "https://orgmode.org/elpa/")
 			   ("elpa" . "https://elpa.gnu.org/packages/")
 			   ("nongnu" . "https://elpa.nongnu.org/nongnu/")))
 (package-initialize)
